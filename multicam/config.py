ee144efd09bf534d8dc1b5f718fce7a4b89686a7
Fahad Mirza <fahad.mirza2@7-11.com>
2025-04-15 23:52:17 -0500

    Updated camera name.

--- multicam/config.py
+++ multicam/config.py
@@ -1,15 +1,14 @@
 """
 The configurations of the application.
 """
-# The cameras are placed in a circle. The position of the camera
-# is used as an alias for the camera to easily identify in the real world.
+# Name the cameras to easily identify them in the real world.
 CAMERAS = {
-    "TWO_O_CLOCK": "010203",
-    "FOUR_O_CLOCK": "040506",
-    "SIX_O_CLOCK": "070809",
-    "EIGHT_O_CLOCK": "101112",
-    "TEN_O_CLOCK": "131415",
-    "TWELVE_O_CLOCK": "161718",
+    "CAM_1": "010203",
+    "CAM_2": "040506",
+    "CAM_3": "070809",
+    "CAM_4": "101112",
+    "CAM_5": "131415",
+    "CAM_6": "161718",
 }
 
 FRAME_RESOLUTION = (1280, 720)
